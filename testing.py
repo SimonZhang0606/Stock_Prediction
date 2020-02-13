8120efcc87df5b01be9fcef4b97a34b83d074543
SimonZhang0606 <58126507+SimonZhang0606@users.noreply.github.com>
2020-02-12 22:17:06 -0500

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
+#modules that i can think of for now
 import math 
 import pandas_datareader as web
 import numpy as np 
@@ -8,5 +9,27 @@ from keras.layers import Dense, LSTM
 import matplotlib.pyplot as plt 
 plt.style.use('fivethirtyeight')
 
+
+#Reading in the data
 df = web.DataReader('AAPL', data_source = 'yahoo', start = "2012-01-01", end = "2019-12-19" )
 print(df)
+
+
+#showing the dashboard
+
+plt.figure(figsize = (16,8))
+plt.title('Close Price History')
+plt.plot(df['Close'])
+plt.xlabel('Date',fontsize=18)
+plt.ylabel('Close Price USD ($)', fontsize=18)
+plt.show()
+
+#Create a new dataframe with only 'Close column'
+
+data = df.filter(['Close'])
+
+#conver the dataframe to a numpy array 
+
+dataset = data.values
+
+
